e45e33fab316a93707d6787f567458f6ba06345c
dheerj18 <dheerajkumar9891.dk@gmail.com1>
2022-02-03 13:42:40 +0530

    make it better

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-document.querySelector(".button").addEventListener("click", function () {
-  document.querySelector(".collapse").classList.remove("collapse");
-  setTimeout(function () {
-    document.querySelector(".collapse-horizontal").classList.add("collapse");
-  }, 10000);
+const express = require("express");
+const app = express();
+app.use(express.static("public"));
+// app.post("/", function (req, res) {
+//   res.querySelector(".button").addEventListener("click", function () {
+//     res.querySelector(".collapse").classList.remove("collapse");
+//     setTimeout(function () {
+//       res.querySelector(".collapse-horizontal").classList.add("collapse");
+//     }, 10000);
+//   });
+// });
+app.get("/", function (req, res) {
+  // res.send("Rendering File");
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(3000 || process.env.PORT, function () {
+  console.log("Server running");
 });
